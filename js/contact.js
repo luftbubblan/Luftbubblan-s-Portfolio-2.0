032b08d3c95190a6164e599994acdf64d8a5a46c
Oscar Blomberg <oscarblomberg@hotmail.com>
2021-12-30 16:25:01 +0100

    email validation

--- js/contact.js
+++ js/contact.js
@@ -8,6 +8,9 @@ contact.addEventListener("submit", function(send){
     const inputEmail     = document.getElementById("email");
     const inputMessage   = document.getElementById("message");
     const errorOutput    = document.getElementById("errorMessage")
+    const questionCheck  = document.getElementById("question")
+    const commentCheck   = document.getElementById("comment")
+    const otherCheck     = document.getElementById("other")
 
     let errorMessage = "";
     if(inputFirstName.value === "") {
@@ -16,9 +19,26 @@ contact.addEventListener("submit", function(send){
     if(inputEmail.value === "") {
         errorMessage += "E-mail is required<br>";
     }
+    if(questionCheck.checked !== true &&
+       commentCheck.checked !== true &&
+       otherCheck.checked !== true) {
+        errorMessage += "Choose if it is a Question, Comment or Other<br>"
+    }
     if(inputMessage.value === "") {
         errorMessage += "A message is required<br>";
     }
+    if(emailValidation(inputEmail) != true) {
+        errorMessage += "Not a complete Email"
+    }
+
+
+
+
+
+
+
+
+
 
     if(errorMessage !== "") {
         errorOutput.innerHTML = `<div class="alert alert-danger" role="alert">
@@ -27,4 +47,27 @@ contact.addEventListener("submit", function(send){
         errorOutput.innerHTML = `<div class="alert alert-success" role="alert">
             Thank you for the message!</div>`;
     }
+
+
+
+
+    function emailValidation(email) {
+        const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (filter.test(email.value)) {
+            return true;
+        } else {
+            return false;}
+        }
+
+
+console.log(emailValidation(inputEmail));
+
+
+
+
+
+
+
+
+
 });
